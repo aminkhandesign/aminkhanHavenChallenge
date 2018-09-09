ae363ce9492f2cc271909f2606961f876dd1554f
Administrator <administrator@LAUSD-MBP-2017-4.local>
2018-09-09 01:14:16 -0700

    FIX ISSUE WITH BABEL-CORE VERSION, SWITCH TO @6 AND BABEL-LOADER TO @7

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 
+const path = require('path')
+
+
 module.exports = {
+   entry : './src/index.js',
+   output: {
+     path:__dirname+'/dist',
+   },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 8080
+      },
     module: {
       rules: [
         {
           test: /\.jsx?$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader"
+            loader: 'babel-loader',
           }
         }
       ]
