427c5cc7eec463ce00cf1570399713d4ebe355a8
Administrator <administrator@LAUSD-MBP-2017-4.local>
2018-09-10 01:25:08 -0700

    FIX RIBBON

--- src/components/left.jsx
+++ src/components/left.jsx
@@ -13,12 +13,13 @@ class Left extends Component {
       return (
           <div className="box left">
           <img src = "./images/left_box/TTR_SWEEP.png"/>
-          <div className="ribbon"><span className="ribbonMsg">Enter for a chance to win a Rock FLight </span>
+          <div className="ribbon"><span className="ribbonMsg">Enter for a chance to win a Rock FLight® </span>
           <span className="ribbonMsg">to a live Nation concert of your choice! </span></div>
+          <div id="ribbonTail"></div>
          < ul className="prizes">1 Grand Prize winner will receive:
          <li id="tickets"><div className="listItem" id="ticket"></div>2 tickets to a Live nation concert in the US*</li>
-         <li id="plane"><div className="listItem" id="ticket"></div>Airfare & hotel accommodation for 2</li>
-         <li id="cash"><div className="listItem" id="ticket"></div>$200 cash gift card</li>
+         <li id="plane"><div className="listItem" id="plane"></div>Airfare & hotel accommodation for 2</li>
+         <li id="cash"><div className="listItem" id="dollar"></div>$200 cash gift card</li>
          </ul>
          </div>
       )
